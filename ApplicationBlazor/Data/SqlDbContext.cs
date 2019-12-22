0c120f0e328aae832c6e9f4665f59658d04168a6
Jean-François DENEUX <jfdeneux@hotmail.fr>
2019-12-22 19:48:57 +0100

    Ajoutez des fichiers projet.

---
+++ ApplicationBlazor/Data/SqlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationBlazor.Data;
+
+namespace ApplicationBlazor.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<ApplicationBlazor.Data.CustomerMaster> CustomerMaster { get; set; }
+    }
+}
